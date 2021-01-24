76ff8acbfd65adb5733f7734fe1160aa137f3556
PeterOsoo <posoo617@gmail.com>
2021-01-24 07:39:50 +0300

    create Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Button from "./Button"
 
 const Header = ({ title }) => {
 	return (
 		<header className="header">
 			<h1>{title}</h1>
+			<Button color="green" text="Add" />
 		</header>
 	)
 }
