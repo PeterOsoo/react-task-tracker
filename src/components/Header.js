04371c104e426a89a2811acd4ceeefc59c0c2665
PeterOsoo <posoo617@gmail.com>
2021-01-24 07:43:57 +0300

    Button onclick & prop types

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import React from "react"
 import Button from "./Button"
 
 const Header = ({ title }) => {
+	const onClick = () => {
+		console.log("clicked button")
+	}
 	return (
 		<header className="header">
 			<h1>{title}</h1>
-			<Button color="green" text="Add" />
+			<Button color="green" text="Add" onClick={onClick} />
 		</header>
 	)
 }
