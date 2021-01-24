3382c54024049d9a04bce83268da42ed9435ec55
PeterOsoo <posoo617@gmail.com>
2021-01-24 07:59:09 +0300

    Global state & useState Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useState } from "react"
+
 import "./App.css"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 
 const App = () => {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "visit doc",
+			day: "feb 4th at 4pm",
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: "go to gym",
+			day: "feb 14th at 2pm",
+			reminder: false,
+		},
+		{
+			id: 1,
+			text: "send mails",
+			day: "may 27th at 8pm",
+			reminder: true,
+		},
+	])
+
 	return (
 		<div className="container">
 			<Header />
-			<Tasks />
+			<Tasks tasks={tasks} />
 		</div>
 	)
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,12 @@
 import React from "react"
 
-const Tasks = () => {
-	const date = new Date()
+const Tasks = ({ tasks }) => {
 	return (
-		<div>
-			<h3>go to shop</h3>
-			<p> {date.getFullYear()} </p>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum sunt
-				similique cupiditate debitis maxime itaque, id odit nostrum ea vero.
-				Saepe voluptatem quas consequatur voluptatum harum inventore facilis
-				laboriosam quos.
-			</p>
-		</div>
+		<>
+			{tasks.map(task => (
+				<h3 key="task.id">{task.text}</h3>
+			))}
+		</>
 	)
 }
 
